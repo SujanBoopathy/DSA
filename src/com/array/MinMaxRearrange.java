f3de1c9635dbdd1cd3ba9c438a0cd3d09b1dbbe1
SujanBoopathy <sujansaradha@gmail.com>
2024-09-13 23:23:30 +0530

    rearrange max min elements

---
+++ src/com/array/MinMaxRearrange.java
@@ -0,0 +1,24 @@
+package com.array;
+
+public class MinMaxRearrange {
+
+    public static void main(String args[]){
+        int[] arr= {1,3,4,5,6,7,78,90,123,455,890,1111,2333,8990};
+
+        int start = 0, end = arr.length -1;
+        int[] tempArray = new int[arr.length];
+        boolean flag = true;
+        for(int i=0;i<arr.length;i++){
+            if(flag){
+                tempArray[i] = arr[end--];
+            } else {
+                tempArray[i] = arr[start++];
+            }
+            flag = !flag;
+        }
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(tempArray[i]+" ");
+        }
+    }
+}
