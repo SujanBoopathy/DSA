6732f5caaa680cfb69b925f18d4b4b029ecb5e6f
SujanBoopathy <sujansaradha@gmail.com>
2024-09-24 20:55:37 +0530

    print outer values

---
+++ src/com/array/OuterElementOfMatrix.java
@@ -0,0 +1,17 @@
+package com.array;
+
+public class OuterElementOfMatrix {
+    public static void main(String args[]){
+        int arr[][] = { {1,2,3,4},
+                        {2,3,4,5},
+                        {3,4,5,6}};
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                if((i==0 || i== arr.length-1) || (j==0 || j == arr[0].length-1)){
+                    System.out.print(" "+arr[i][j]);
+                }
+            }
+        }
+    }
+}
