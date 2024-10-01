46be0ccddd35e6faae55307dbddb16d88e0108d7
SujanBoopathy <sujansaradha@gmail.com>
2024-10-01 21:31:59 +0530

    rearrange the array

---
+++ src/com/array/RearrangeArray.java
@@ -0,0 +1,15 @@
+package com.array;
+
+public class RearrangeArray {
+    private static void fixArray(int arr[],int n){
+
+    }
+
+
+    public static void main(String args[]){
+        int n, ar[] = { -1, -1, 6, 1, 9, 3, 2, -1, 4, -1 };
+        n = ar.length;
+        // Function Call
+        fixArray(ar, n);
+    }
+}
